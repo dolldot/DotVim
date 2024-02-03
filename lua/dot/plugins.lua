fff9848eed299bf8aba8751555d4ea93518a7e22
aliyul4jis <aliyul.ajis@fazzfinancial.com>
2024-02-04 00:08:21 +0700

    update plugins and add ChatGPT plugin

--- lua/dot/plugins.lua
+++ lua/dot/plugins.lua
@@ -225,6 +225,19 @@ local plugins = {
     name = "catppuccin",
     priority = 1000
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
 }
 -- LuaFormatter on
 require("lazy").setup(plugins)
