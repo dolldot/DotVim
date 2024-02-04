265500b1f07812360a38a9212e37d1973ecfa636
aliyul4jis <aliyul.ajis@fazzfinancial.com>
2024-02-04 16:09:57 +0700

    feat(neotree): auto resize window when spliting

--- lua/dot/config/neotree.lua
+++ lua/dot/config/neotree.lua
@@ -1,6 +1,38 @@
 local M = {}
 
-_G.dotneotreeconfig = { window = { position = "left" } }
+_G.dotneotreeconfig = {
+  window = { position = "left" },
+  event_handlers = {
+    -- {
+    --  event = "neo_tree_window_before_open",
+    --  handler = function(args)
+    --    print("neo_tree_window_before_open", vim.inspect(args))
+    --  end
+    -- },
+    {
+      event = "neo_tree_window_after_open",
+      handler = function(args)
+        if args.position == "left" or args.position == "right" then
+          vim.cmd("wincmd =")
+        end
+      end,
+    },
+    -- {
+    --  event = "neo_tree_window_before_close",
+    --  handler = function(args)
+    --    print("neo_tree_window_before_close", vim.inspect(args))
+    --  end
+    -- },
+    {
+      event = "neo_tree_window_after_close",
+      handler = function(args)
+        if args.position == "left" or args.position == "right" then
+          vim.cmd("wincmd =")
+        end
+      end,
+    },
+  },
+}
 
 M.setup = function()
   local ok, neotree = pcall(require, "neo-tree")
