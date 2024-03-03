221cc234e4193ed04c04dd90d3f0f7674f2c71e0
aliyul4jis <aliyul.ajis@fazzfinancial.com>
2024-03-03 19:04:02 +0700

    chore: move keybindings to the module configuration

--- lua/dot/config/chatgpt.lua
+++ lua/dot/config/chatgpt.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local config = {
+  api_key_cmd = "python3 /Users/dolldot/Mine/dolldot/project/paperknife/scripts/crypt.py decrypt 5ByaXxRH+Eqke9Dls3dhWHgdf3FMfL45nsl/uJKVAnm27PdqFyOjzy+VS6SUUTnNp4IEh/5oHpD4aXEVuzJO1w==",
   openai_params = {
     model = "gpt-3.5-turbo",
     -- model = "gpt-4",
@@ -26,6 +27,23 @@ M.setup = function()
   if (not status) then
     return
   end
+
+  local utils = require("dot.utils")
+  local dmap = utils.set_keymapd
+
+  dmap({ "n", "v" }, "<Leader>C", "<cmd>ChatGPT<CR>", "ChatGPT")
+  dmap({ "n", "v" }, "<Leader>Ce", "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction")
+  dmap({ "n", "v" }, "<Leader>Cg", "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction")
+  dmap({ "n", "v" }, "<Leader>Ct", "<cmd>ChatGPTRun translate<CR>", "Translate")
+  dmap({ "n", "v" }, "<Leader>Ck", "<cmd>ChatGPTRun keywords<CR>", "Keywords")
+  dmap({ "n", "v" }, "<Leader>Cd", "<cmd>ChatGPTRun docstring<CR>", "Docstring")
+  dmap({ "n", "v" }, "<Leader>Ca", "<cmd>ChatGPTRun add_tests<CR>", "Add Tests")
+  dmap({ "n", "v" }, "<Leader>Co", "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code")
+  dmap({ "n", "v" }, "<Leader>Cs", "<cmd>ChatGPTRun summarize<CR>", "Summarize")
+  dmap({ "n", "v" }, "<Leader>Cf", "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs")
+  dmap({ "n", "v" }, "<Leader>Cx", "<cmd>ChatGPTRun explain_code<CR>", "Explain Code")
+  dmap({ "n", "v" }, "<Leader>Cr", "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit")
+  dmap({ "n", "v" }, "<Leader>Cl", "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis")
 
   chatgpt.setup(config)
 end
--- lua/dot/config/telescope.lua
+++ lua/dot/config/telescope.lua
@@ -11,6 +11,23 @@ M.setup = function()
     return
   end
 
+  local utils = require("dot.utils")
+  local nmap = utils.set_keymapn
+
+  -- Telescope
+  local builtin = require "telescope.builtin"
+  nmap("<leader>fh", builtin.help_tags, "[S]earch [H]elp")
+  nmap("<leader>fk", builtin.keymaps, "[S]earch [K]eymaps")
+  nmap("<leader>ff", builtin.find_files, "[S]earch [F]iles")
+  nmap("<leader>fs", builtin.builtin, "[S]earch [S]elect Telescope")
+  nmap("<leader>fw", builtin.grep_string, "[S]earch current [W]ord")
+  nmap("<leader>fg", builtin.live_grep, "[S]earch by [G]rep")
+  nmap("<leader>fd", builtin.diagnostics, "[S]earch [D]iagnostics")
+  nmap("<leader>fr", builtin.resume, "[S]earch [R]esume")
+  nmap("<leader>f.", builtin.oldfiles, "[S]earch Recent Files (\".\" for repeat)")
+  nmap("<leader><leader>", builtin.buffers, "[ ] Find existing buffers")
+
+  telescope.load_extension("fzf")
   telescope.setup(config)
 end
 
