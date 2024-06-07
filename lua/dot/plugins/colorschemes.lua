57af7683968161b3ba722bfefca25a85dd5ba5cf
aliyul4jis <aliyul.ajis@fazzfinancial.com>
2024-06-07 18:38:09 +0700

    feat: add more colorschemes

--- lua/dot/plugins/colorschemes.lua
+++ lua/dot/plugins/colorschemes.lua
@@ -5,6 +5,9 @@ return {
   "rose-pine/neovim",
   "AlexvZyl/nordic.nvim",
   "navarasu/onedark.nvim",
+  "sainnhe/gruvbox-material",
+  "sainnhe/everforest",
+  "sainnhe/sonokai",
   {
     "catppuccin/nvim",
     name = "catppuccin",
