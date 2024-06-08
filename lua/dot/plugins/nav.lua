f01a3977f716bded38344572f74fcede8a579d25
aliyul4jis <aliyul.ajis@fazzfinancial.com>
2024-06-08 20:55:05 +0700

    chore: update harpoon keymaps

--- lua/dot/plugins/nav.lua
+++ lua/dot/plugins/nav.lua
@@ -10,17 +10,17 @@ return {
       -- REQUIRED
 
       vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():append()
+        harpoon:list():add()
       end)
       vim.keymap.set("n", "<C-e>", function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end)
 
       -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<C-P>", function()
+      vim.keymap.set("n", "<C-S-P>", function()
         harpoon:list():prev()
       end)
-      vim.keymap.set("n", "<C-N>", function()
+      vim.keymap.set("n", "<C-S-N>", function()
         harpoon:list():next()
       end)
     end,
